cbd9140fff090d8cc3cb5788b92ab3b910a38ea0
Failed-Captcha <154801175+Failed-Captcha2@users.noreply.github.com>
2023-12-25 17:33:09 -0500

    fixed resolution, added rudimentary walking mechanics and collision

--- the son/Assets/Scripts/resolution.cs
+++ the son/Assets/Scripts/resolution.cs
@@ -6,9 +6,10 @@ public class resolution : MonoBehaviour
 {
     public Camera main_camera;
     // Start is called before the first frame update
+    
     void Start()
     {
-        
+       Screen.SetResolution(2200,900,true);
     }
 
     // Update is called once per frame

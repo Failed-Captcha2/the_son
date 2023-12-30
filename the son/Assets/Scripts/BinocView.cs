505c5c47c8760b2e9461678cc4c95a4786a2e006
Failed-Captcha <154801175+Failed-Captcha2@users.noreply.github.com>
2023-12-29 23:11:56 -0500

    updated binoc view mechanics
    -movement in binoc view
    -distance limit in binoc view
    -fixed issue with pixelation decreasing
    -added more birds
    -addded text (might make a custom font ??)

--- the son/Assets/Scripts/BinocView.cs
+++ the son/Assets/Scripts/BinocView.cs
@@ -7,13 +7,19 @@ public class BinocView : MonoBehaviour
     public Transform cameraPos;
     public Material pixelate;
     public float pixelationSpeed;
+    public float cameraSpeed;
+    public SpriteRenderer bird;
 
     private Transform binocViewPos;
+    private Vector3 move;
+    private Vector3 originalPos;
     public bool birdInView;
+    public float maxDistance;
     // Start is called before the first frame update
     void Start()
     {
         binocViewPos = GetComponent<Transform>();
+        originalPos = binocViewPos.position;
         gameObject.SetActive(false);
         pixelate.SetFloat("_Pixelate", 2);
     }
@@ -22,8 +28,45 @@ public class BinocView : MonoBehaviour
     void Update()
     {
         binocViewPos.transform.position = cameraPos.position + new Vector3(0, 0, 2); //folow camera with binoc view screen
-        if (birdInView) { pixelate.SetFloat("_Pixelate", pixelate.GetFloat("_Pixelate") + pixelationSpeed); } //increase pixels if bird is within view
-        else { pixelate.SetFloat("_Pixelate", pixelate.GetFloat("_Pixelate") - pixelationSpeed); } //decrease pixels if bird is out of view
+        if (birdInView && pixelate.GetFloat("_Pixelate") < 40) { pixelate.SetFloat("_Pixelate", pixelate.GetFloat("_Pixelate") + pixelationSpeed); } //increase pixels if bird is within view
+        else if (!birdInView && pixelate.GetFloat("_Pixelate") > 2) { pixelate.SetFloat("_Pixelate", pixelate.GetFloat("_Pixelate") - pixelationSpeed); } //decrease pixels if bird is out of view
+
+        move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")); //key input
+        if (!move.Equals(Vector3.zero)) { moveCamera(); }
+    }
+
+    void OnMouseDrag() //touchscreen/mouse input
+    {
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        //get x direction
+        if (worldPosition.x < binocViewPos.position.x - 0.2) { move.x = -1; }
+        else if (worldPosition.x > binocViewPos.position.x + 0.2) { move.x = 1; }
+
+        //get y direction
+        if (worldPosition.y < binocViewPos.position.y - 0.2) { move.y = -1; }
+        else if (worldPosition.y > binocViewPos.position.y + 0.2) { move.y = 1; }
+
+        moveCamera();
+    }
+
+    void moveCamera() //move binoc view
+    {
+        //if reached max or min x change don't move in y direction
+        if (binocViewPos.position.x + move.x * cameraSpeed * Time.deltaTime - originalPos.x > maxDistance ||
+                originalPos.x - binocViewPos.position.x - move.x * cameraSpeed * Time.deltaTime > maxDistance)
+        { move.x = 0; }
+
+        //if reached max or min  y don't move in y direction
+        if (binocViewPos.position.y + move.y * cameraSpeed * Time.deltaTime - originalPos.y > maxDistance ||
+        originalPos.y - binocViewPos.position.y - move.y * cameraSpeed * Time.deltaTime > maxDistance)
+        { move.y = 0; }
+
+        //move 
+        move = move * Time.deltaTime * cameraSpeed;
+        binocViewPos.transform.position = new Vector3(binocViewPos.position.x + move.x, binocViewPos.position.y + move.y, binocViewPos.position.z);
+        cameraPos.transform.position = new Vector3(binocViewPos.position.x, binocViewPos.position.y, cameraPos.position.z);
+
     }
 
     void OnTriggerEnter2D(Collider2D other) { birdInView = true; }
--- the son/Assets/Scripts/PlayerController.cs
+++ the son/Assets/Scripts/PlayerController.cs
@@ -29,53 +29,52 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {   
+    {
         //key input
         move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")); //converst key input into vector
 
         //mouse input
-         if(Input.GetMouseButton(0)){
+        if (Input.GetMouseButton(0))
+        {
             Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            
-            if (worldPosition.x< player.transform.position.x-0.2){
-                move.x = -1;
-            }else if (worldPosition.x> player.transform.position.x+0.2){
-                move.x = 1;
-            }
-            if (worldPosition.y< player.transform.position.y-0.2){
-                move.y = -1;
-            }else if (worldPosition.y> player.transform.position.y+0.2){
-                move.y = 1;
-            }
+
+            if (worldPosition.x < player.position.x - 0.2) { move.x = -1; }
+            else if (worldPosition.x > player.position.x + 0.2) { move.x = 1; }
+
+            if (worldPosition.y < player.position.y - 0.2) { move.y = -1; }
+            else if (worldPosition.y > player.position.y + 0.2) { move.y = 1; }
         }
 
         //animation 
-        if((!move.Equals(Vector3.zero)||Input.GetMouseButton(0))&&!walking){
-                animator.Play("walking",0,0);
-                walking = true;
+        if ((!move.Equals(Vector3.zero) || Input.GetMouseButton(0)) && !walking)
+        {
+            animator.Play("walking", 0, 0);
+            walking = true;
 
-        }else if(move.Equals(Vector3.zero) && !Input.GetMouseButton(0) && walking){
-            animator.Play("standing",0,0);
+        }
+        else if (move.Equals(Vector3.zero) && !Input.GetMouseButton(0) && walking)
+        {
+            animator.Play("standing", 0, 0);
             sprite.sprite = defaultSprite;
             walking = false;
         }
 
-        if(playSound){sound.Play();} //walking audio
+        if (playSound) { sound.Play(); } //walking audio
 
         //flip sprite
-        if(move.x<0 && sprite.flipX){
+        if (move.x < 0 && sprite.flipX)
+        {
             sprite.flipX = false;
-        }else if(move.x>0 && !sprite.flipX){
+        }
+        else if (move.x > 0 && !sprite.flipX)
+        {
             sprite.flipX = true;
         }
-        
+
         //change position
-        move = move*playerSpeed;
+        move = move * playerSpeed;
         playerRB.velocity = move;
-        player.transform.position = new Vector3(player.position.x,player.position.y,player.position.y);
-            // move.x = player.position.x + move.x;
-            // move.y = player.position.y + move.y;
-            // move.z = move.y;
-            // player.transform.position = move;
+        player.transform.position = new Vector3(player.position.x, player.position.y, player.position.y);
+
     }
 }
